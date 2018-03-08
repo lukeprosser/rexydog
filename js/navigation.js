27dfdf4c3157c75eac704288d3cb062f64f3b82a
lukeprosser <lukesprosser@gmail.com>
2018-03-08 17:20:26 +0000

    CSS & JS comments

--- js/navigation.js
+++ js/navigation.js
@@ -30,7 +30,7 @@ jQuery(document).ready(function() {
       var hash = this.hash;
 
       // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      // The optional number (800) indicates the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
